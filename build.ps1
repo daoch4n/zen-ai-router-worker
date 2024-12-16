13457e1b63118efd4bae54921479ed7bae508145
vadash <vadash@gmail.com>
2024-12-16 23:52:49 +0300

    Disable obfuscate

--- build.ps1
+++ build.ps1
@@ -1,8 +1,7 @@
 try {
     Remove-Item -Recurse -Force -Path .\output\
     npx wrangler deploy --dry-run --outdir output
-    npx terser output\worker.js -o output\worker.js --comments false
-    node .\obfuscate.mjs
+    npx terser output\worker.js -o output\_worker.js --comments false
     $randomGuid = [guid]::NewGuid().ToString()
     Compress-Archive -Path .\output\_worker.js -DestinationPath ".\output\worker-$randomGuid.zip" -Force
 } catch {
