59db88fcdfc579df10c9ecae9c345e448a56f898
vadash <vadash@gmail.com>
2025-01-07 21:30:06 +0300

    Update builder

--- build.ps1
+++ build.ps1
@@ -1,9 +1,5 @@
 try {
-    Remove-Item -Recurse -Force -Path .\output\
-    npx wrangler deploy --dry-run --outdir output
-    npx terser output\worker.js -o output\_worker.js --comments false
-    $randomGuid = [guid]::NewGuid().ToString()
-    Compress-Archive -Path .\output\_worker.js -DestinationPath ".\output\worker-$randomGuid.zip" -Force
+    npx wrangler deploy
 } catch {
     Write-Host "An error occurred: $_"
     pause
